170f04a4a1d00b400445d50f056deab77ab42fd7
melda-gonen-vinci <melda.gonen@student.vinci.be>
2024-09-24 15:18:40 +0200

    new: ex 2.1-destructing

--- exercises/module2/2.1/part1/src/components/ClickCounter/ClickCounter.jsx
+++ exercises/module2/2.1/part1/src/components/ClickCounter/ClickCounter.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-function ClickCounter() {
+
+const ClickCounter = ({title, on10clicks = "You are a master in the art of clicking !"}) => {
     const [count, setCount] = useState(0)
     return (
         <div>
             <button onClick={() => setCount(count + 1)}>
                 count is {count}
+                {count >= 10 ? <p>{on10clicks}</p>  : title}
             </button>
         </div>
     )
