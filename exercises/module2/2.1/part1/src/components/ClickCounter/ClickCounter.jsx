6af86c8e95f716c8c8f0bf10ccc3a1f793fbf98a
melda-gonen-vinci <melda.gonen@student.vinci.be>
2024-09-24 15:04:09 +0200

    new: ex 2.1-init

---
+++ exercises/module2/2.1/part1/src/components/ClickCounter/ClickCounter.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function ClickCounter() {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>
+                count is {count}
+            </button>
+        </div>
+    )
+
+}
+export default ClickCounter
