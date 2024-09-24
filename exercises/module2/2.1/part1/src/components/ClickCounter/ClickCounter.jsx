48d0f34201fb6862d4b38d906c30cb5bed89d82d
melda-gonen-vinci <melda.gonen@student.vinci.be>
2024-09-24 15:58:41 +0200

    new:ex2.1-stored

--- exercises/module2/2.1/part1/src/components/ClickCounter/ClickCounter.jsx
+++ exercises/module2/2.1/part1/src/components/ClickCounter/ClickCounter.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 
 const ClickCounter = ({title, on10clicks = "You are a master in the art of clicking !", message = "Please click on me now !"}) => {
-    const [count, setCount] = useState(0)
+    const storedCount = JSON.parse(localStorage.getItem("count"))
+    const [count, setCount] = useState(storedCount)
     const [onButton, setButton] = useState(false)
+    
 
     return (
         <div>
             {onButton ? <p>{message}</p> : null}
-            <button onClick={() => setCount(count + 1)}
+            <button onClick={() => {
+            const newCount = count + 1
+            setCount(newCount)
+            localStorage.setItem("count", newCount)
+            }
+            }
+            
             onMouseEnter={() => setButton(true)}
             onMouseLeave={() => setButton(false)}
             >
