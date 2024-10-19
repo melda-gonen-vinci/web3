1fe714d1e19138e349a9eefc310241300ea2ab1b
melda-gonen-vinci <melda.gonen@student.vinci.be>
2024-10-19 17:11:29 +0200

    6.2: crud op

---
+++ exercises/module6/6.2/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const personsRoutes = require('./routes/persons')
+const {errorHandler, disableCors} = require('./utils/middlewares')
+const {MONGODB_URI, PORT} = require('./utils/config')
+
+// Connect to database
+mongoose.connect(MONGODB_URI)
+console.log('Connected to MongoDB');
+
+// Create and init server
+const app = express()
+
+app.use(express.json())
+app.use(disableCors)
+app.use('/persons', personsRoutes)
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
